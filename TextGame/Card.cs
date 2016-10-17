35b7c074b28a3463efd02f0553cbf1ddafa653f2
Kevin Thomas <kevint@magenic.com>
2016-10-17 14:47:09 -0500

    Starting next level

---
+++ TextGame/Card.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    class Card
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public int Cost { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+
+        public bool Battlecry { get; set; }
+        public bool DeathRattle { get; set; }
+        public bool Adjacent { get; set; }
+        public bool Aura { get; set; }
+        public bool Taunt { get; set; }
+        public bool Stealth { get; set; }
+
+        public Card(string name)
+        {
+            Name = name;
+
+            Text = "";
+            Battlecry = false;
+            DeathRattle = false;
+            Adjacent = false;
+            Aura = false;
+            Taunt = false;
+            Stealth = false;
+
+            switch (Name)
+            {
+                case "Chicken":
+                    Cost = 1;
+                    Attack = 1;
+                    Health = 1;
+                    break;
+                case "Stampeding Kodo":
+                    Text = "Battlecry: Destroy a random enemy minion with 2 or less attack";
+                    Cost = 5;
+                    Attack = 3;
+                    Health = 5;
+                    Battlecry = true;
+                    break;
+                case "Strangulthorn Tiger":
+                    Text = "Stealth";
+                    Cost = 5;
+                    Attack = 5;
+                    Health = 5;
+                    Stealth = true;
+                    break;
+                case "Dire Wolf Alpha":
+                    Text = "Adjacent minions have +1 attack";
+                    Cost = 2;
+                    Attack = 2;
+                    Health = 2;
+                    Adjacent = true;
+                    break;
+            }
+        }
+    }
+}
---
+++ TextGame/CardGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    class CardGame : Scenario
+    {
+        Thread musicThread = new Thread(PlayMusic);
+
+        List<Card> _playerCards = new List<Card>();
+        List<Card> _enemyCards = new List<Card>();
+
+        public CardGame(Player player) : base(player)
+        {
+            _player = player;
+            musicThread.Start();
+        }
+
+        public static void PlayMusic()
+        {
+            Assembly assembly;
+            SoundPlayer sp;
+            assembly = Assembly.GetExecutingAssembly();
+            sp = new SoundPlayer(assembly.GetManifestResourceStream
+                ("TextGame.audio.night.wav"));
+            sp.Stream.Position = 0;
+            sp.PlayLooping();
+        }
+
+        public void Start()
+        {
+            //TODO
+            StartTurn();
+        }
+
+        public void StartTurn()
+        {
+            //TODO
+        }
+    }
+}
